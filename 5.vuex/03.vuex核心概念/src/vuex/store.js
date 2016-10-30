264cfd91a9fbb0f78525ddbed5720296f50ca721
stiekel <histkc@gmail.com>
2016-10-30 18:43:35 +0800

    06.vuex 中间件

--- 5.vuex/03.vuex核心概念/src/vuex/store.js
+++ 5.vuex/03.vuex核心概念/src/vuex/store.js
@@ -1,17 +1,27 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import mutations from './mutations'
+import lodash from 'lodash'
 
 Vue.use(Vuex)
 
 const state = {
   count: Math.ceil(100 * Math.random()),
   isRequesting: false
+const showLogs = store => {
+  var prevState = lodash.cloneDeep(store.state)
+  store.subscribe((mutation, state) => {
+    state.count++
+    console.log('mutation', mutation, 'state', state)
+    console.log('new count:', state.count, 'prev count:', prevState.count)
+    prevState = lodash.cloneDeep(store.state)
+  })
 }
 
 const store = new Vuex.Store({
   state,
-  mutations
+  mutations,
+  plugins: [ showLogs ]
 })
 
 export default store
